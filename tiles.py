4d582076f348005799ca9f30176b2b7442fa3c84
rolux <rolux@rolux.org>
2025-07-12 09:21:12 +0200

    new tileset

--- tiles.py
+++ tiles.py
@@ -27,6 +27,7 @@ maps = [
 ]
 overlays = [
     ("aiwe", 1, aiwe_scale, aiwe_zero),
+    ("martipk", 5, 0.558, (2232, -2232)),
     ("rickrick", 2, 2.5, (5000, 7500)),
     ("vlad", 1, 1.674, (3000 * 1.674, -3000 * 1.674)),
 ]
