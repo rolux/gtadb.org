28c3d536151c60c8fda6737ab6c69404960b745e
rolux <rolux@rolux.org>
2025-07-24 08:45:12 +0200

    refactor api, add gta5

--- map/api/api.py
+++ map/api/api.py
@@ -14,6 +14,11 @@ from PIL import Image
 
 
 
+class APIError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+# FIXME: this is somewhat duplicated
 class LM(IntEnum):
     IG_ADDRESS = 0
     IG_COORDINATES = 1
@@ -24,6 +29,117 @@ class LM(IntEnum):
     TAGS = 6
     COLOR = 7
     LAST_EDITED = 8
+
+LANDMARK_KEYS = [
+    "ig_address", "ig_coordinates", "ig_photo",
+    "rl_address", "rl_coordinates", "rl_photo",
+    "tags"
+]
+
+
+
+def add_user(username, password):
+    users = read_json(USERS_FILE)
+    profile_color = get_color(username)
+    users[username] = {"password_hash": get_hash(password), "profile_color": profile_color}
+    write_json(USERS_FILE, users)
+
+def validate_invite_code(invite_code):
+    invites = read_json(INVITES_FILE)
+    return any(
+        test_against_hash(invite_code, invite_hash)
+        for invite_hash in invites
+    )
+
+def remove_invite(invite_code):
+    invites = read_json(INVITES_FILE)
+    invites = {
+        invite_hash: data
+        for invite_hash, data in invites.items()
+        if not test_against_hash(invite_code, invite_hash)
+    }
+    write_json(INVITES_FILE, invites)
+
+def change_password(username, new_password):
+    users = read_json(USERS_FILE)
+    users[username]["password_hash"] = get_hash(new_password)
+    write_json(USERS_FILE, users)
+
+
+def create_session(username):
+    sessions = read_json(SESSIONS_FILE)
+    session_id = uuid.uuid4().hex
+    sessions[session_id] = {"time": time.time(), "user": username}
+    write_json(SESSIONS_FILE, sessions)
+    return session_id
+
+def get_user(session_id):
+    sessions = read_json(SESSIONS_FILE)
+    return sessions.get(session_id, {}).get("user")
+
+def remove_session(session_id):
+    sessions = read_json(SESSIONS_FILE)
+    sessions.pop(session_id, None)
+    write_json(SESSIONS_FILE, sessions)
+
+
+def add_landmark(game, ig_coordinates, username):
+    landmarks = read_json(LANDMARKS_FILE[game])
+    last_id = list(landmarks.keys())[-1]
+    landmark_id = f"x{int(last_id[1:]) + 1}"
+    timestamp = time.time()
+    landmarks[landmark_id] = [
+        "?", ig_coordinates, [],
+        "?", [], [],
+        [], get_landmark_color("?"), [timestamp, 0, 0]
+    ]
+    write_log(game, [timestamp, username, "add_landmark", landmark_id, "ig_coordinates", ig_coordinates])
+    write_landmarks(game, landmarks)
+    return landmark_id, landmarks[landmark_id]
+
+def edit_landmark(game, landmark_id, key, value, file, username):
+    landmarks = read_json(LANDMARKS_FILE[game])
+    if landmark_id not in landmarks:
+        raise APIError("Unknown landmark ID")
+    index = LANDMARK_KEYS.index(key)
+    if key in ("ig_photo", "rl_photo"):
+        if not file:
+            landmarks[landmark_id][index] = []
+            source = key.split("_")[0]
+            remove_photo(f"{PHOTOS_DIR}/{landmark_id},{source}.jpg")
+        else:
+            source = key.split("_")[0]
+            filename = f"{PHOTOS_DIR}/{landmark_id},{source}.jpg"
+            image = Image.open(file.stream).convert("RGB")
+            image.save(filename)
+            value = image.size
+            landmarks[landmark_id][index] = value
+    else:
+        value = check_landmark_data(key, value)
+        landmarks[landmark_id][index] = value
+        if key == "rl_address":
+            landmarks[landmark_id][LM.RL_COORDINATES] = get_coordinates(value)
+            landmarks[landmark_id][LM.COLOR] = get_landmark_color(value)
+    last_edited = landmarks[landmark_id][LM.LAST_EDITED]
+    timestamp = time.time()
+    landmarks[landmark_id][LM.LAST_EDITED] = [
+        int(timestamp),
+        int(timestamp) if key == "ig_photo" else last_edited[1],
+        int(timestamp) if key == "rl_photo" else last_edited[2]
+    ]
+    write_log(game, [timestamp, username, "edit_landmark", landmark_id, key, value])
+    write_landmarks(game, landmarks)
+    return landmarks[landmark_id]
+
+def remove_landmark(game, landmark_id, username):
+    landmarks = read_json(LANDMARKS_FILE[game])
+    if landmark_id not in landmarks:
+        raise APIError("Unknown landmark ID")
+    del landmarks[landmark_id]
+    write_log(game, [time.time(), username, "remove_landmark", landmark_id, None, None])
+    write_landmarks(game, landmarks)
+    for source in ("ig", "rl"):
+        remove_photo(f"{PHOTOS_DIR}/{landmark_id},{source}.jpg")
 
 def check_landmark_data(key, value):
     if key == "ig_address":
@@ -42,49 +158,6 @@ def check_landmark_data(key, value):
     if key == "tags":
         return sorted(list(set(tag.lower() for tag in value)))
 
-def create_session(username):
-    session_id = uuid.uuid4().hex
-    sessions[session_id] = {"time": time.time(), "user": username}
-    write_json(SESSIONS_FILE, sessions)
-    return session_id
-
-def get_color(name):
-    sha1 = hashlib.sha1(name.encode("utf-8")).hexdigest()[-6:]
-    rgb = tuple(int(int(sha1[i * 2:i * 2 + 2], 16) * 0.75) for i in range(3))
-    return "".join(f"{v:02x}" for v in rgb)
-
-def get_coordinates(address):
-    if address and address not in geodata:
-        result = gmaps.geocode(address)
-        geodata[address] = result
-        write_json(GEODATA_FILE, geodata)
-    if not geodata[address]:
-        print(f"### NO GEODATA FOR {address} ###")
-        return []
-    location = geodata[address][0]["geometry"]["location"]
-    return location["lat"], location["lng"]
-
-def get_hash(string):
-    salt = os.urandom(16)
-    hash = hashlib.pbkdf2_hmac("sha256", string.encode(), salt, 100_000)
-    return base64.b64encode(salt + hash).decode()
-
-def get_landmark_color(address):
-    name = "???" if address in ("", "?") else address.split(", ")[0]
-    for _ in range(3):
-        name = re.sub(" \\([A-Z0-9\\?]+\\)$", "", name)
-        if name[-1] != ")":
-            break
-    return get_color(name)
-
-def get_new_landmark_id(landmarks):
-    last_id = list(landmarks.keys())[-1]
-    return f"x{int(last_id[1:]) + 1}"
-
-def read_json(filename):
-    with open(filename, "r") as f:
-        return json.load(f)
-
 def remove_photo(filename):
     if os.path.exists(filename):
         filename_new = filename.replace(
@@ -94,19 +167,61 @@ def remove_photo(filename):
         )
         os.replace(filename, filename_new)
 
+
+
+def get_landmarks_since(game, since):
+    since = float(since)
+    with open(LOG_FILE[game]) as f:
+        landmarks = {}
+        for line in f:
+            timestamp, user, action, landmark_id, data = json.loads(line)
+            if timestamp >= since:
+                landmarks[landmark_id] = data
+    return landmarks
+
+def get_coordinates(address):
+    if address and address not in geodata:
+        result = gmaps.geocode(address)
+        geodata[address] = result
+        write_json(GEODATA_FILE, geodata)
+    if not geodata[address]:
+        return []
+    location = geodata[address][0]["geometry"]["location"]
+    return location["lat"], location["lng"]
+
+def get_color(name):
+    sha1 = hashlib.sha1(name.encode("utf-8")).hexdigest()[-6:]
+    rgb = tuple(int(int(sha1[i * 2:i * 2 + 2], 16) * 0.75) for i in range(3))
+    return "".join(f"{v:02x}" for v in rgb)
+
+def get_landmark_color(address):
+    name = "???" if address in ("", "?") else address.split(", ")[0]
+    for _ in range(3):
+        name = re.sub(" \\([A-Z0-9\\?]+\\)$", "", name)
+        if name[-1] != ")":
+            break
+    return get_color(name)
+
+def get_profile_color(username):
+    users = read_json(USERS_FILE)
+    return users[username]["profile_color"] if username in users else get_color("???")
+
+
+def get_hash(string):
+    salt = os.urandom(16)
+    hash = hashlib.pbkdf2_hmac("sha256", string.encode(), salt, 100_000)
+    return base64.b64encode(salt + hash).decode()
+
 def test_against_hash(string, stored_hash):
     decoded = base64.b64decode(stored_hash)
     salt, stored_hash = decoded[:16], decoded[16:]
     test_hash = hashlib.pbkdf2_hmac("sha256", string.encode(), salt, 100_000)
     return hmac.compare_digest(stored_hash, test_hash)
 
-def write_file(filename, data):
-    filename_tmp = f"{filename}.tmp"
-    with open(filename_tmp, "w") as f:
-        fcntl.flock(f, fcntl.LOCK_EX)
-        f.write(data)
-        fcntl.flock(f, fcntl.LOCK_UN)
-    os.replace(filename_tmp, filename)
+
+def read_json(filename):
+    with open(filename, "r") as f:
+        return json.load(f)
 
 def write_json(filename, data, sort_fn=lambda kv: kv):
     jdata = "{\n"
@@ -116,11 +231,19 @@ def write_json(filename, data, sort_fn=lambda kv: kv):
     jdata += "}"
     write_file(filename, jdata)
 
-def write_landmarks(landmarks):
-    write_json(LANDMARKS_FILE, landmarks, sort_fn=lambda kv: (kv[0][0], int(kv[0][1:])))
+def write_file(filename, data):
+    filename_tmp = f"{filename}.tmp"
+    with open(filename_tmp, "w") as f:
+        fcntl.flock(f, fcntl.LOCK_EX)
+        f.write(data)
+        fcntl.flock(f, fcntl.LOCK_UN)
+    os.replace(filename_tmp, filename)
 
-def write_log(item):
-    with open(LOG_FILE, "a") as f:
+def write_landmarks(game, landmarks):
+    write_json(LANDMARKS_FILE[game], landmarks, sort_fn=lambda kv: (kv[0][0], int(kv[0][1:])))
+
+def write_log(game, item):
+    with open(LOG_FILE[game], "a") as f:
         f.write(json.dumps(item) + "\n")
 
 
@@ -136,15 +259,24 @@ API_KEY_FILE = f"{DATA_DIR}/google_maps_api_key.txt"
 CONFIG_FILE = f"{DATA_DIR}/config.json"
 GEODATA_FILE = f"{DATA_DIR}/geodata.json"
 INVITES_FILE = f"{DATA_DIR}/invites.json"
-LANDMARKS_FILE = f"{DATA_DIR}/landmarks.json"
-LOG_FILE = f"{DATA_DIR}/edit_log.jsonl"
+LANDMARKS_FILE = {
+    5: f"{DATA_DIR}/5/landmarks.json",
+    6: f"{DATA_DIR}/6/landmarks.json"
+}
+LOG_FILE = {
+    5: f"{DATA_DIR}/5/edit_log.jsonl",
+    6: f"{DATA_DIR}/6/edit_log.jsonl"
+}
 SESSIONS_FILE = f"{DATA_DIR}/sessions.json"
 USERS_FILE = f"{DATA_DIR}/users.json"
 
 config = read_json(CONFIG_FILE)
 geodata = read_json(GEODATA_FILE)
 invites = read_json(INVITES_FILE)
-landmarks = read_json(LANDMARKS_FILE)
+landmarks = {
+    5: read_json(LANDMARKS_FILE[5]),
+    6: read_json(LANDMARKS_FILE[6])
+}
 sessions = read_json(SESSIONS_FILE)
 users = read_json(USERS_FILE)
 gmaps = googlemaps.Client(key=config["GOOGLE_MAPS_API_KEY"])
@@ -153,13 +285,6 @@ gmaps = googlemaps.Client(key=config["GOOGLE_MAPS_API_KEY"])
 
 @app.route("/api", methods=["POST"])
 def api():
-
-    # multiple workers, update for every call
-    geodata = read_json(GEODATA_FILE)
-    invites = read_json(INVITES_FILE)
-    landmarks = read_json(LANDMARKS_FILE)
-    sessions = read_json(SESSIONS_FILE)
-    users = read_json(USERS_FILE)
 
     if request.content_type and request.content_type.startswith("multipart/form-data"):
         req = request.form.to_dict()
@@ -172,7 +297,7 @@ def api():
 
     # print(f"{req=}")
     session_id = request.cookies.get("session_id")
-    user = sessions.get(session_id, {}).get("user")
+    user = get_user(session_id)
     action = req.get("action")
     invite_code = req.get("invite_code")
     username = req.get("username")
@@ -182,6 +307,7 @@ def api():
     new_password = req.get("new_password")
     repeat_new_password = req.get("repeat_new_password")
     since = req.get("since")
+    game = req.get("game")
     landmark_id = req.get("id")
     key = req.get("key")
     value = req.get("value")
@@ -198,21 +324,18 @@ def api():
         return {"status": "error", "message": "Unauthorized request"}
 
     if action in (
-        "edit_landmark", "remove_landmark"
-    ) and landmark_id not in list(landmarks.keys()):
-        return {"status": "error", "message": "Unknown landmark ID"}
+        "get_landmarks", "add_landmark", "edit_landmark", "remove_landmark"
+    ) and game not in (5, 6):
+        return {"status": "error", "message": "unknown game version"}
 
     if action == "get_user":
-        profile_color = users[user]["profile_color"] if user in users else get_color("???")
+        profile_color = get_profile_color(user)
         return {"status": "ok", "username": user, "session_id": session_id, "profile_color": profile_color}
 
     if action == "create_account":
         if not invite_code:
             return {"status": "error", "message": "Missing invite code"}
-        if not any(
-            test_against_hash(invite_code, invite_hash)
-            for invite_hash in invites
-        ):
+        if not validate_invite_code(invite_code):
             return {"status": "error", "message": "Invalid invite code"}
         if not username or not re.match("^[\\w-]+$", username):
             return {"status": "error", "message": "Invalid username"}
@@ -222,15 +345,8 @@ def api():
             return {"status": "error", "message": "Missing password"}
         if password != repeat_password:
             return {"status": "error", "message": "Passwords do not match"}
-        invites = {
-            invite_hash: data
-            for invite_hash, data in invites.items()
-            if not test_against_hash(invite_code, invite_hash)
-        }
-        write_json(INVITES_FILE, invites)
-        profile_color = get_color(username)
-        users[username] = {"password_hash": get_hash(password), "profile_color": profile_color}
-        write_json(USERS_FILE, users)
+        remove_invite(invite_code)
+        add_user(username, password)
         session_id = create_session(username)
         response = make_response({
             "status": "ok",
@@ -254,7 +370,7 @@ def api():
         if not test_against_hash(password, users[username]["password_hash"]):
             return {"status": "error", "message": "Invalid credentials"}
         session_id = create_session(username)
-        profile_color = users[username]["profile_color"]
+        profile_color = get_profile_color(username)
         response = make_response({
             "status": "ok",
             "username": username,
@@ -278,88 +394,43 @@ def api():
             return {"status": "error", "message": "Invalid credentials"}
         if new_password != repeat_new_password:
             return {"status": "error", "message": "Passwords do not match"}
-        users[username]["password_hash"] = get_hash(new_password)
-        write_json(USERS_FILE, users)
+        change_password(username, new_password)
         return {"status": "ok"}
 
     if action == "logout":
-        sessions.pop(session_id, None)
-        write_json(SESSIONS_FILE, sessions)
+        remove_session(session_id)
         return {"status": "ok"}
 
     if action == "get_landmarks":
-        if since is None or not os.path.exists(LOG_FILE):
-            return {"status": "ok", "landmarks": landmarks}
-        since = float(since)
-        with open(LOG_FILE) as f:
-            edited = {}
-            for line in f:
-                e_timestamp, e_user, e_action, e_id, e_data = json.loads(line)
-                if e_timestamp >= since and e_user != user:
-                    edited[e_id] = e_data
-        return {"status": "ok", "landmarks": edited}
+        if since is None or not os.path.exists(LOG_FILE[game]):
+            return {"status": "ok", "landmarks": landmarks[game]}
+        landmarks = get_landmarks_since(game, since)
+        return {"status": "ok", "landmarks": landmarks}
 
     if action == "add_landmark":
         if key != "ig_coordinates" or not value:
             return {"status": "error", "message": "missing in-game coordinates"}
-        landmark_id = get_new_landmark_id(landmarks)
-        timestamp = time.time()
-        landmarks[landmark_id] = [
-            "?", value, [],
-            "?", [], [],
-            [], get_landmark_color("?"), [timestamp, 0, 0]
-        ]
-        write_log([timestamp, user, action, landmark_id, key, value])
-        write_landmarks(landmarks)
-        return {"status": "ok", "id": landmark_id, "data": landmarks[landmark_id]}
+        landmark_id, landmark = add_landmark(game, value, user)
+        return {"status": "ok", "id": landmark_id, "data": landmark}
 
     if action == "edit_landmark":
-        landmark_keys = (
-            "ig_address", "ig_coordinates", "ig_photo",
-            "rl_address", "rl_coordinates", "rl_photo",
-            "tags"
-        )
-        if key not in landmark_keys:
+        if key not in LANDMARK_KEYS:
             return {"status": "error", "message": "Unknown landmark key"}
-        index = landmark_keys.index(key)
-        if key in ("ig_photo", "rl_photo"):
-            if not file:
-                landmarks[landmark_id][index] = []
-                source = key.split("_")[0]
-                remove_photo(f"{PHOTOS_DIR}/{landmark_id},{source}.jpg")
-            else:
-                ext = file.filename.split(".")[-1].lower()
-                if ext not in ("jpeg", "jpg", "png", "webp"):
-                    return {"status": "error", "message": "Unsupported image file type"}
-                source = key.split("_")[0]
-                filename = f"{PHOTOS_DIR}/{landmark_id},{source}.jpg"
-                image = Image.open(file.stream).convert("RGB")
-                image.save(filename)
-                value = image.size
-                landmarks[landmark_id][index] = value
-        else:
-            value = check_landmark_data(key, value)
-            landmarks[landmark_id][index] = value
-            if key == "rl_address":
-                landmarks[landmark_id][LM.RL_COORDINATES] = get_coordinates(value)
-                landmarks[landmark_id][LM.COLOR] = get_landmark_color(value)
-        last_edited = landmarks[landmark_id][LM.LAST_EDITED]
-        timestamp = time.time()
-        landmarks[landmark_id][LM.LAST_EDITED] = [
-            int(timestamp),
-            int(timestamp) if key == "ig_photo" else last_edited[1],
-            int(timestamp) if key == "rl_photo" else last_edited[2]
-        ]
-        write_log([timestamp, user, action, landmark_id, key, value])
-        write_landmarks(landmarks)
-        return {"status": "ok", "data": landmarks[landmark_id]}
+        if file and file.filename.split(".")[-1].lower() not in (
+            "jpeg", "jpg", "png", "webp"
+        ):
+            return {"status": "error", "message": "Unsupported image file type"}
+        try:
+            landmark = edit_landmark(game, landmark_id, key, value, file, user)
+        except APIError:
+            return {"status": "error", "message": "Unknown landmark ID"}
+        return {"status": "ok", "data": landmark}
 
     if action == "remove_landmark":
-        write_log([time.time(), user, action, landmark_id, None, None])
-        del landmarks[landmark_id]
-        write_landmarks(landmarks)
-        for source in ("ig", "rl"):
-            remove_photo(f"{PHOTOS_DIR}/{landmark_id},{source}.jpg")
+        try:
+            remove_landmark(game, landmark_id, user)
+        except APIError:
+            return {"status": "error", "message": "Unknown landmark ID"}
         return {"status": "ok"}
 
     return {"status": "error", "message": "Invalid request"}
@@ -373,7 +444,7 @@ if __name__ == "__main__":
 
     INDEX_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
-    # TODO: Disable these routes in producttion (handled by caddy)
+    # TODO: Disable these routes in production (handled by caddy)
 
     @app.route("/data/<path:filename>")
     def serve_data(filename):
@@ -391,9 +462,6 @@ if __name__ == "__main__":
     @app.route("/", defaults={"path": ""})
     @app.route("/<path:path>")
     def serve_static(path):
-        if path == "":
-            path = "index.html"
-        print("Serving static:", os.path.join(INDEX_DIR, path))
-        return send_from_directory(INDEX_DIR, path)
+        return send_from_directory(INDEX_DIR, path or "index.html")
 
     app.run(host="0.0.0.0", port=8080, debug=False)
